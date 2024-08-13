e305da13a04736b738204d5804c13b95df6f9dcf
Nvm35 <76003751+Nvm35@users.noreply.github.com>
2024-08-13 19:53:32 +0500

    fixing

--- Source/SlashingThings/Private/Items/Item.cpp
+++ Source/SlashingThings/Private/Items/Item.cpp
@@ -4,13 +4,14 @@
 AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Cyan, FString("Checking screen"));
+	}
 }
 
 // Called every frame
@@ -18,5 +19,11 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
+
+	if (GEngine) {
+		FString Message = FString::Printf(TEXT("DeltaTIIIME: %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Cyan, Message);
+	}
 }
 
