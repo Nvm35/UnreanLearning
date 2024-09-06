07be1897d63b5634f8bd22d4817f3a09afb471e0
Nvm35 <76003751+Nvm35@users.noreply.github.com>
2024-09-06 19:10:19 +0500

    speed

---
+++ Source/SlashingThings/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashAnimInstance.h"
+#include "Characters/SlashChatacters.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	SlashCharacter = Cast<ASlashChatacters>(TryGetPawnOwner());
+
+
+	if (SlashCharacter)
+	{
+		SlashCharacterMovement = SlashCharacter->GetCharacterMovement();
+		/*bIsInAir = SlashCharacter->GetCharacterMovement()->IsFalling();
+		bIsAccelerating = SlashCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0;*/
+	}
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (SlashCharacterMovement)
+	{
+		Speed = UKismetMathLibrary::VSizeXY(SlashCharacterMovement->Velocity);
+	}
+
+}
---
+++ Source/SlashingThings/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+
+UCLASS()
+class SLASHINGTHINGS_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class ASlashChatacters* SlashCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	class UCharacterMovementComponent* SlashCharacterMovement;
+
+	// Переменные для анимации, такие как скорость и направление движения
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsAccelerating;
+};
